a3f46947b68acb357494b54e841119a19210baf8
HenryCai0317 <henrytsai@cathaysec.com.tw>
2023-12-08 17:16:02 +0800

    [Home Work] init Project

---
+++ app/src/main/java/com/example/gogolookhomework/view/PhotoSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gogolookhomework.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PhotoSearchActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
